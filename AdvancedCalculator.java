a3659fa76bac2b51d6a7cad98452026ef5c6c08e
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2025-02-28 17:00:55 +0200

    inheritance

---
+++ AdvancedCalculator.java
@@ -0,0 +1,5 @@
+public class AdvancedCalculator extends Calculator {
+    public int remainder(int n1, int n2) {
+        return n1 % n2;
+    }
+}
--- Hello.java
+++ Hello.java
@@ -5,11 +5,27 @@ class Hello {
 
         // Class.forName("Mobile");
 
-        Human human = new Human();
-        human.setAge(27);
-        human.setName("Aubin");
+        // Human human = new Human();
+        // human.setAge(27);
+        // human.setName("Aubin");
 
-        System.out.println("Hello, my name is: " + human.getName() + " and I am: " + human.getAge() + " years old!");
+        // Human humanOne = new Human("Kari Nordmann");
+        // humanOne.setAge(21);
+
+        // System.out.println("Hello, my name is: " + human.getName() + " and I am: " +
+        // human.getAge() + " years old!");
+        // System.out.println("Hello, my name is: " + humanOne.getName() + " and I am: "
+        // + humanOne.getAge() + " years old!");
+
+        // Dog dog = new Dog("bark");
+
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        int sum = calculator.add(36, 8);
+        int difference = calculator.substract(16, 2);
+        int product = calculator.multiply(9, 9);
+        int quotient = calculator.divide(16, 4);
+        int remainder = calculator.remainder(33, 32);
+        System.out.println(remainder);
     }
 }
 
@@ -35,10 +51,19 @@ class Human {
     private int age;
     protected String name;
 
+    public Human() {
+        age = 18;
+        name = "Ola Nordmann";
+    }
+
+    public Human(String name) {
+        this.name = name;
+    }
+
     public int getAge() {
         return age;
     }
-    
+
     public void setAge(int age) {
         this.age = age;
     }
@@ -49,5 +74,27 @@ class Human {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+class Animal {
+    public Animal() {
+        System.out.println("in Animal");
+    }
+
+    public Animal(String sound) {
+        System.out.println("in Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    public Dog() {
+        super();
+        System.out.println("in Dog");
+    }
+
+    public Dog(String sound) {
+        this();
+        System.out.println("in Dog sound");
     }
 }
